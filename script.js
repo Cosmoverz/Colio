22dc17f3137226f3c968d7731aab300f7905cb28
Cooli <74840262+Spacesters@users.noreply.github.com>
2022-01-30 12:53:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 // Disable Right Clicking
 document.addEventListener('contextmenu', event => event.preventDefault());
-// Disable pressing on any keys
-document.onkeydown = function(e) {
-        if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {}
-        return false;
-};
+// Get the current year
+document.getElementById("year").innerHTML = new Date().getFullYear();
