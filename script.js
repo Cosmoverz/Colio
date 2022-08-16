c5c79ebee776e4c2c03c2ec70696946fe124e8be
Cosmoverse <74840262+Spacesters@users.noreply.github.com>
2022-08-16 15:29:09 -0500

    Remove 'Disabling right click' feature

--- script.js
+++ script.js
@@ -1,4 +1,2 @@
-// Disable Right Clicking
-document.addEventListener('contextmenu', event => event.preventDefault());
 // Get the current year
 document.getElementById("year").innerHTML = new Date().getFullYear();
